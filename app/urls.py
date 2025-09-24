eb7a993446a73e7b0be05be7759752790aab4ad7
Theo Svensson <theosvensson5@gmail.com>
2025-09-24 15:00:17 +0200

    Uploading Zip Works

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
     path('report/', views.report, name='report'),
     path('history/', views.history, name='history'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    uploaded_file_url = None
+    
+    
+    if request.method == "POST" and request.FILES.get("myZip"):
+        myzip = request.FILES["myZip"]
+        
+        fs = FileSystemStorage()
+        filename = fs.save(myzip.name, myzip)
+        uploaded_file_url = fs.url(filename)
+        return render(request, "home.html") #, {"uploaded_file_url": uploaded_file_url})
+    
+    return render(request, 'home.html')
 
 def report(request):
     return HttpResponse("This is the Report page")
