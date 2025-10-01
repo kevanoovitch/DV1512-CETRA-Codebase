a4ae1ea05adb4e8414389e4c742665ee9987081e
lambda-tron <fajeralhamwi@proton.me>
2025-10-01 15:38:29 +0200

    virustotal + unittest

--- app/backend/API_interfaces/VirusTotalInterface.py
+++ app/backend/API_interfaces/VirusTotalInterface.py
@@ -4,8 +4,6 @@ import json
 from dotenv import load_dotenv
 import os
 
-##start of code
-
 load_dotenv()
 
 ApiKey = os.getenv("VT_API_KEY")
@@ -17,32 +15,45 @@ base_url = "https://www.virustotal.com/api/v3/files/upload_url"
 
 HEADERS = { "x-apikey": ApiKey}
 
-file_path = "../uploaded/"
+directory_path = "../uploaded/"
 
 
-def scan_file(file_name: str):
-    headers = {"x-apikey": ApiKey}
+def scan_file(file_name: str, debug=False):
+    try:
+        file_path = file_name if debug else directory_path + file_name
+        
+        headers = {"x-apikey": ApiKey}
 
-    with open(file_path, "rb") as f:
-        files = {"file": (file_path+file_name, f)}
-        response = requests.post(
-            "https://www.virustotal.com/api/v3/files",
-            headers=headers,
-            files=files
-        )
-    response.raise_for_status()
-    analysis_id = response.json()["data"]["id"]
+        with open(file_path, "rb") as f:
+            files = {"file": (file_path, f)}
+            response = requests.post(
+                "https://www.virustotal.com/api/v3/files",
+                headers=headers,
+                files=files
+            )
+        response.raise_for_status()
+        analysis_id = response.json()["data"]["id"]
 
-    analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+        analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+        
+        timeout = 30
+        print("loading data from Virus total")
+        while timeout > 0:
+            res = requests.get(analysis_url, headers=headers)
+            res.raise_for_status()
+            data = res.json()
+            status = data["data"]["attributes"]["status"]
+            if status == "completed":
+                return analyse_data(data["data"])
 
-    while True:
-        res = requests.get(analysis_url, headers=headers)
-        res.raise_for_status()
-        data = res.json()
-        status = data["data"]["attributes"]["status"]
+            time.sleep(1)
+            timeout -= 1
 
-        if status == "completed":
-            return analyse_data(data["data"])
+        return None
+    except Exception as e:
+        print(f"[scan_file] Error: {e}")
+        return None
+    
 
 def analyse_data(result):
     data = {}
@@ -50,7 +61,19 @@ def analyse_data(result):
     data = {"stats":result["attributes"]["status"],}
     for engine,details in result["attributes"]["results"].items():
         if(details["category"] != "undetected" and details["category"] != "type-unsupported"):
-            detectedby.add(details)
+            detectedby.append(details)
     stats = result["attributes"]["stats"]
-    
-    return  {"stats":stats, "detectedby":detectedby}
+    return  {"stats":stats, "detectedby":detectedby,"score":calculate_malicious_score(stats)}
+
+def calculate_malicious_score(stats: dict) -> int:
+    malicious = stats.get("malicious", 0)
+    suspicious = stats.get("suspicious", 0)
+
+    total = malicious + suspicious + stats.get("undetected", 0) + stats.get("harmless", 0)
+    if total == 0:
+        return 0
+
+    score = (2 * malicious + 1 * suspicious) / (2 * total) * 100
+    return round(score)
+
+
