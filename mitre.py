d7b6586746358d90d03d98f5a05cddf367b805dd
TheoMASvensson <theosvensson5@gmail.com>
2025-11-12 12:49:00 +0100

    mitre from VT

--- mitre.py
+++ mitre.py
@@ -1,11 +1,27 @@
 #fetch hash
-from app.backend.report_generator import file_hash  
-hash = file_hash
+# from app.backend.report_generator import file_hash  
 
 #get curl command
 import requests
-headerAccept = {'accept: application/json'}
-headerAPI = {'x-apikey: {VT_API_KEY}'}
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-response = requests.get("https://www.virustotal.com/api/v3/files/{hash}/behaviour_mitre_trees", headerAccept, headerAPI)
+hash = "0efc314b1b7f6c74e772eb1f8f207ed50c2e702aed5e565081cbcf8f28f0fe26"
+url = f"https://www.virustotal.com/api/v3/files/{hash}/behaviour_mitre_trees"
+
+headers = {
+    "accept": "application/json",
+    "x-apikey": os.getenv("VT_API_KEY")
+    
+}
+#exists = requests.get(f"https://www.virustotal.com/api/v3/files/{hash}", headers=headers)
+
+response = requests.get(url, headers=headers)
+
+print(response.status_code)
 print(response.json())
+
+#print(exists.status_code)
+#print(exists.json())
+
