fdcbd4949d66186c5a8291eb50c887391d6b48ba
Kabbe51 <kasperthunell@gmail.com>
2025-11-13 13:02:12 +0100

    Update mitre.py

--- app/backend/mitreFolder/mitre.py
+++ app/backend/mitreFolder/mitre.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-hash = "0efc314b1b7f6c74e772eb1f8f207ed50c2e702aed5e565081cbcf8f28f0fe26"
+filehash = "0efc314b1b7f6c74e772eb1f8f207ed50c2e702aed5e565081cbcf8f28f0fe26"
 url = f"https://www.virustotal.com/api/v3/files/{hash}/behaviour_mitre_trees"
 
 headers = {
@@ -15,13 +15,30 @@ headers = {
     "x-apikey": os.getenv("VT_API_KEY")
     
 }
-#exists = requests.get(f"https://www.virustotal.com/api/v3/files/{hash}", headers=headers)
 
 response = requests.get(url, headers=headers)
 
 print(response.status_code)
 print(response.json())
 
+def mitre_report(filehash, response) -> dict:
+
+    filehasher = [filehash]
+    sandbox = response["sandbox_name"]
+    tactics = response["tactic_id"]["tactic_name"]
+    techniques = response["technique_id"]["technique_name"]
+
+    parsed_response = {
+        "filehash": filehasher,
+        "sandbox": sandbox,
+        "tactics": tactics,
+        "techniques": techniques,
+    }
+    return parsed_response
+
+#exists = requests.get(f"https://www.virustotal.com/api/v3/files/{hash}", headers=headers)
+
+
+
 #print(exists.status_code)
 #print(exists.json())
-
