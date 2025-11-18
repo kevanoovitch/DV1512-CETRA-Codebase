046ee42a5b9f11dee9bf98556fb67971ee936f6b
kevanoovitch <kede23@student.bth.se>
2025-11-18 15:10:08 +0100

    Fix: Zip now work but won't extract a ID, tests reflects this now.

--- app/tests/integration_tests/test_integration_VT.py
+++ app/tests/integration_tests/test_integration_VT.py
@@ -1,7 +1,14 @@
 import unittest
 from app.backend.API_interfaces.VirusTotalInterface import scan_file
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("VT_API_KEY")
 
 class TestVirusTotalScan(unittest.TestCase):
+    @unittest.skipUnless(API_KEY, "Skipping VirusTotal scan: API key not set.")
     def test_scan_returns_result(self):
         test_file = "app/tests/test_crx/mil.crx"
         result = scan_file(test_file)
